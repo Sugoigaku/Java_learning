ec02a27dbfca032c61bc48fefcf707fac47e43a2
Sugoigaku <hakbean0728@gmail.com>
2022-04-11 23:44:40 +0900

    mp update

--- mybatis_plus_learning/mp-springboot/src/test/java/com/chen/mp/UserMapperTest.java
+++ mybatis_plus_learning/mp-springboot/src/test/java/com/chen/mp/UserMapperTest.java
@@ -1,8 +1,12 @@
 package com.chen.mp;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
 import com.chen.mp.mapper.UserMapper;
 import com.chen.mp.pojo.User;
+import org.apache.ibatis.annotations.Param;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,5 +51,42 @@ public class UserMapperTest {
     @Test
     public void testSelectOne(){
         System.out.println(userMapper.selectById(3));
+    }
+
+//    int updateById(@Param(Constants.ENTITY) T entity);
+//    实例中设置id，方法通过该id进行更新操作
+    @Test
+    public void testUpdateById(){
+        User user = new User();
+        user.setId(1L);
+        user.setAge(19);
+
+        int result = userMapper.updateById(user);
+        System.out.println(result);
+    }
+
+    /**
+     *  根据 whereEntity 条件，更新记录
+     *  entity 实体对象 (set 条件值,可以为 null)
+     *  updateWrapper 实体对象封装操作类（可以为 null,里面的 entity 用于生成 where 语句） */
+//    int update(@Param(Constants.ENTITY) T entity, @Param(Constants.WRAPPER) Wrapper<T> updateWrapper);
+    @Test
+    public void testUpdate(){
+//        更新条件1 用QueryWrapper包装类
+        QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
+//      equal方法  第一个参数是表的字段名
+        userQueryWrapper.eq("id", 2L);
+
+        //        更新条件2 用UpdateWrapper包装类
+        UpdateWrapper<User> updateWrapper = new UpdateWrapper<>();
+//      equal方法  第一个参数是表的字段名
+        updateWrapper.eq("id", 3L);
+
+        User user = new User();
+        user.setAge(1032);
+        user.setPassword("899999");
+
+        int result = userMapper.update(user, updateWrapper);
+        System.out.println(result);
     }
 }
