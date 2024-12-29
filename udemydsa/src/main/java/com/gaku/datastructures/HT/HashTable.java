7b494d4dd335685cacb80a2f235afa73ad19cc83
XUEBIN Chen <xuebin.chen@xuebinc-ltmdhzx.internal.salesforce.com>
2024-12-29 21:11:36 +0900

    HashTable

--- udemydsa/src/main/java/com/gaku/datastructures/HT/HashTable.java
+++ udemydsa/src/main/java/com/gaku/datastructures/HT/HashTable.java
@@ -1,16 +1,18 @@
 package com.gaku.datastructures.HT;
 
+import java.util.ArrayList;
+
 public class HashTable {
 
     private int size = 7;
-    Node[] dataMap;
+    private Node[] dataMap;
 
     class Node {
         String key;
         int value;
         Node next;
-        
-        public Node(String key, int value) {
+
+        Node(String key, int value) {
             this.key = key;
             this.value = value;
         }
@@ -25,15 +27,78 @@ public class HashTable {
     }
 
     public void printTable() {
-        for(int i = 0; i < dataMap.length; i++) {
+        for (int i = 0; i < dataMap.length; i++) {
             System.out.println(i + ":");
-            if(dataMap[i] != null) {
-                Node temp = dataMap[i];
-                while (temp != null) {
-                    System.out.println("   {" + temp.key + ", " + temp.value + "}");
-                    temp = temp.next;
-                }
+            Node temp = dataMap[i];
+            while (temp != null) {
+                System.out.println("   {" + temp.key + "= " + temp.value + "}");
+                temp = temp.next;
             }
         }
+    }
+
+    private int hash(String key) {
+        int hash = 0;
+        char[] keyChars = key.toCharArray();
+        for (int i = 0; i < keyChars.length; i++) {
+            int asciiValue = keyChars[i];
+            hash = (hash + asciiValue * 23) % dataMap.length;
+        }
+        return hash;
+    }
+
+    public void set(String key, int value) {
+        int index = hash(key);
+
+        Node newNode = new Node(key, value);
+
+        if (dataMap[index] == null) {
+            dataMap[index] = newNode;
+            return;
+        }
+
+        Node temp = dataMap[index];
+
+        while (temp.next != null) {
+            if (key.equals(temp.key)) {
+                temp.value = value;
+                return;
+            }
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+    }
+
+    public int get(String key) {
+        int index = hash(key);
+
+        if (dataMap[index] == null)
+            return 0;
+
+        Node temp = dataMap[index];
+
+        while (temp != null) {
+            if (key.equals(temp.key)) return temp.value;
+            temp = temp.next;
+        }
+
+        return 0;
+    }
+
+    public ArrayList<String> keys() {
+        ArrayList<String> allKeys = new ArrayList<String>();
+
+        Node temp;
+        for (int i = 0; i < dataMap.length; i++) {
+            temp = dataMap[i];
+            if (temp == null) continue;
+            while (temp != null) {
+                allKeys.add(temp.key);
+                temp = temp.next;
+            }
+        }
+
+        return allKeys;
     }
 }
--- udemydsa/src/main/java/com/gaku/datastructures/HT/Main.java
+++ udemydsa/src/main/java/com/gaku/datastructures/HT/Main.java
@@ -5,6 +5,20 @@ public class Main {
         
         HashTable myHashTable = new HashTable();
 
+        myHashTable.set("nails", 100);
+        myHashTable.set("tile", 50);
+        myHashTable.set("lumber", 80);
+        // myHashTable.set("bolts", 200);
+        myHashTable.set("screws", 140);
+
         myHashTable.printTable();
+
+        System.out.println("Lumber:");
+        System.out.println( myHashTable.get("lumber") );
+
+        System.out.println("\nBolts:");
+        System.out.println( myHashTable.get("bolts") );
+
+        System.out.println( myHashTable.keys() );
     }
 }
